1f64475e668e8f8564d792ba3d9d2c4ef765ed9b
Larissa Batista <102837475+larabatista8@users.noreply.github.com>
2026-07-22 23:18:10 +0000

    configuraçao dos pinos

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print("Sistema Kanban Inicializado")
+from machine import ADC, Pin
+import time
+
+#configuracao dos pinos
+ldr_pino = ADC(Pin(34))
+ldr_pino.atten(ADC.ATTN_11DB)
+btn_pino = Pin(12,Pin.IN, Pin.PULL_UP)
+
+#----------------
+total_pecas = 0
+peca_bloqueando = False
+tempo_inicio_bloqueio = 0
+alerta_parada_disparado= False
+
+print("Contador de Producao Inicializado")
