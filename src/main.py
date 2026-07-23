f9d765ca76bf18c9a39d4a36d5497fae55824d2f
Larissa Batista <102837475+larabatista8@users.noreply.github.com>
2026-07-23 21:05:49 +0000

    implementando funções para deteccao de peças e reset de turno

--- src/main.py
+++ src/main.py
@@ -1,15 +1,67 @@
-from machine import ADC, Pin
 import time
+from machine import Pin, ADC
+
+
+def detecta_peca(leitura_ldr, tempo_atual):
+    global peca_em_transito, tempo_inicio_bloqueio, alerta_parada_disparado, total_pecas
+    if leitura_ldr > LIMIAR_BLOQUEIO:
+        if peca_em_transito == False:
+            peca_em_transito = True
+            tempo_inicio_bloqueio = tempo_atual
+            alerta_parada_disparado = False
+        else:
+            # verifica tempo de bloqueio
+            if not alerta_parada_disparado and time.ticks_diff(tempo_atual, tempo_inicio_bloqueio) >= TEMPO_PARADA_MS:
+                print("Alerta: Micro-parada detectada!")
+                alerta_parada_disparado = True
+
+    elif leitura_ldr < LIMIAR_LIVRE:
+        if peca_em_transito:
+            total_pecas += 1
+            print(f"Peca detectada! Total: {total_pecas}")
+            peca_em_transito = False
+
+
+def reseta_turno(leitura_btn, tempo_atual):
+    global ultimo_estado_btn, ultimo_tempo_btn, total_pecas, peca_em_transito, alerta_parada_disparado
+    if leitura_btn != ultimo_estado_btn:
+        ultimo_tempo_btn = tempo_atual
+        ultimo_estado_btn = leitura_btn
+
+    if time.ticks_diff(tempo_atual, ultimo_tempo_btn) > 50:
+        if leitura_btn == 0 and ultimo_estado_btn == 0:  
+            total_pecas = 0
+            peca_em_transito = False
+            alerta_parada_disparado = False
+            print("Turno resetado com sucesso. Contadores zerados.")
 
 #configuracao dos pinos
 ldr_pino = ADC(Pin(34))
 ldr_pino.atten(ADC.ATTN_11DB)
 btn_pino = Pin(12,Pin.IN, Pin.PULL_UP)
 
-#----------------
+# constantes 
+LIMIAR_BLOQUEIO = 1800     # valor para lux baixo
+LIMIAR_LIVRE = 1000        # valor para  lux alto
+TEMPO_PARADA_MS = 5000     # 5 segundos para alerta de micro-parada
+
+#variaveis para ajudar a analisar o movimento da esteira
 total_pecas = 0
-peca_bloqueando = False
+peca_em_transito = False
 tempo_inicio_bloqueio = 0
 alerta_parada_disparado= False
 
+# debounce do botao
+ultimo_estado_btn = 1
+ultimo_tempo_btn = 0
+
 print("Contador de Producao Inicializado")
+
+while True:
+    tempo_atual = time.ticks_ms()
+    leitura_ldr = ldr_pino.read()
+    leitura_btn = btn_pino.value()
+    detecta_peca(leitura_ldr, tempo_atual)
+    reseta_turno(leitura_btn, tempo_atual)
+    #tempo para estabilidade
+    time.sleep_ms(10)  
